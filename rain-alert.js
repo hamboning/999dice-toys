0599ae56e5f6c1c3fc6c9b1b5b6074f68506a42e
toplel <hamboning@users.noreply.github.com>
2014-08-12 23:11:46 +0300

    Update rain-alert.js

--- rain-alert.js
+++ rain-alert.js
@@ -5,24 +5,25 @@
 
 
 if (location.hostname == 'www.999dice.com') {
-	var audio = {};
-	audio["alert"] = new Audio();
-	audio["alert"].src = "https://cdn.rawgit.com/hamboning/999dice-toys/master/codec.wav";
-
+	
 	function soundz() {
-		audio["alert"].play();
+		window.audio = {};
+		audio["alert"] = new Audio();
+		audio["alert"].src = "https://cdn.rawgit.com/hamboning/999dice-toys/master/codec.wav";
+		window.audio["alert"].play();
+		return false;
 	} 
-
+	
 	$("#ChatTabChatContainer").bind("DOMSubtreeModified",function(){
 		el = $(this);
-
+	
 		lastmsg = el.children().last('.ChatText').text();
 		BtcRainButton = el.children().last('.ChatText').find('.TextButton').text();
-
+	
 		checkRain = (/BTC/i.test(BtcRainButton));
 		soundtest = (/Ancient Psychic Tandem War Elephant/i.test(lastmsg));
 		
 		if (( checkRain ) || ( soundtest ))
-				{ soundz(); }
+			{ soundz(); }
 	});	
 }
